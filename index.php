3745ab108c0a19d853bcba0c04a0f0ff74fb091c
MuballighHossain <65614878+MuballighHossain@users.noreply.github.com>
2023-01-15 19:10:40 +0600

    changes-1

--- index.php
+++ index.php
@@ -239,22 +239,7 @@
 <!-- Container for demo purpose -->
 
 
-<div class="bg-gray-50">
-    <div class="px-6 py-12 mx-auto max-w-7xl lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            <span class="block">Ready to dive in?</span>
-            <span class="block text-indigo-600">Start exploring the Cloud today.</span>
-        </h2>
-        <div class="flex mt-8 lg:mt-0 lg:flex-shrink-0">
-            <div class="inline-flex rounded-md shadow">
-                <a href="https://www.huaweicloud.com/intl/en-us/product/" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700">Products</a>
-            </div>
-            <div class="inline-flex ml-3 rounded-md shadow">
-                <a href="https://www.huaweicloud.com/intl/en-us/solution/" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-indigo-600 bg-white border border-transparent rounded-md hover:bg-indigo-50">Solutions</a>
-            </div>
-        </div>
-    </div>
-</div>
+
 
 
 
@@ -341,6 +326,23 @@
 
 
 
+
+<div class="bg-gray-50">
+    <div class="px-6 py-12 mx-auto max-w-7xl lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            <span class="block">Ready to dive in?</span>
+            <span class="block text-indigo-600">Start exploring the Cloud today.</span>
+        </h2>
+        <div class="flex mt-8 lg:mt-0 lg:flex-shrink-0">
+            <div class="inline-flex rounded-md shadow">
+                <a href="https://www.huaweicloud.com/intl/en-us/product/" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-700">Products</a>
+            </div>
+            <div class="inline-flex ml-3 rounded-md shadow">
+                <a href="https://www.huaweicloud.com/intl/en-us/solution/" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-indigo-600 bg-white border border-transparent rounded-md hover:bg-indigo-50">Solutions</a>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <hr class="h-px my-8 bg-gray-300 border-0 ">
